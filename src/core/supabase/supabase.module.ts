a1379c0d265153b2c1e8a6b89c9cc46da710d4ee
amfajardoo <amfajardoo94@gmail.com>
2025-09-09 22:38:51 -0500

    refactor: restructure Auth module and service, update import paths

--- src/core/supabase/supabase.module.ts
+++ src/core/supabase/supabase.module.ts
@@ -1,4 +1,4 @@
-import { AuthModule } from '@core/auth/auth/auth.module';
+import { AuthModule } from '@core/auth/auth.module';
 import { Global, Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
 import { SupabaseAuthGuard } from './supabase-auth.guard';
